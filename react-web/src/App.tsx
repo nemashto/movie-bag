3f15da93389188e14e194fe59287b37d1c474153
nemashto <ne.mash.to@gmail.com>
2021-12-06 11:49:13 +0100

    added footer

--- react-web/src/App.tsx
+++ react-web/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { MovieList } from './pages/MoviePages/MoviesList';
 import { Header } from './common/components/header';
+import { Footer } from './common/components/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MovieList />
+      <Footer />
     </div>
   );
 }
