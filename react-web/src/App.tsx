afe31d64cdc79379d6a6076f905735614cca8cfb
nemashto <ne.mash.to@gmail.com>
2021-12-06 11:42:33 +0100

    added bootstrap, router and header

--- react-web/src/App.tsx
+++ react-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
 import { MovieList } from './pages/MoviePages/MoviesList';
+import { Header } from './common/components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList />
     </div>
   );
