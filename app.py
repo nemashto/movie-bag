5479a346ea55caa3bc6c989f681b15ec40d2e44a
nemashto <ne.mash.to@gmail.com>
2021-12-13 11:55:24 +0100

    backend login

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
-from database.db import initialize_db
 from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+from database.db import initialize_db
 from resources.routes import initialize_routes
 
 app = Flask(__name__)
+app.config.from_envvar('ENV_FILE_LOCATION')
+
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 # init
 app.config['MONGODB_SETTINGS'] = {
--- database/models.py
+++ database/models.py
@@ -1,7 +1,19 @@
 from .db import db
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 
 class Movie(db.Document):
     name = db.StringField(required=True, unique=True)
     casts = db.ListField(db.StringField())
     genres = db.ListField(db.StringField())
+
+
+class User(db.Document):
+    email = db.EmailField(required = True, unique = True)
+    password = db.StringField(required = True, min_length=6)
+
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
