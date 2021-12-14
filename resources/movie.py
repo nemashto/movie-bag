6d3f3b0260f54ce41d44beffbe4402ceef0cc2e0
nemashto <ne.mash.to@gmail.com>
2021-12-14 21:12:10 +0100

    auth by author

--- resources/movie.py
+++ resources/movie.py
@@ -1,7 +1,7 @@
 from flask import request, Response
-from database.models import Movie
+from database.models import Movie, User
 from flask_restful import Resource
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 class MoviesApi(Resource):
@@ -11,8 +11,13 @@ class MoviesApi(Resource):
 
     @jwt_required()
     def post(self):
+        user_id = get_jwt_identity()
         body = request.get_json()
-        movie = Movie(**body).save()
+        user = User.objects.get(id=user_id)
+        movie = Movie(**body, added_by=user)
+        movie.save()
+        user.update(push__movies=movie)
+        user.save()
         id = movie.id
         return {'id': str(id)}, 200
 
@@ -20,13 +25,17 @@ class MoviesApi(Resource):
 class MovieApi(Resource):
     @jwt_required()
     def put(self, id):
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)       
         body = request.get_json()
-        Movie.objects.get(id=id).update(**body)
+        movie.update(**body)
         return '', 200
 
     @jwt_required()
     def delete(self, id):
-        Movie.objects.get(id=id).delete()
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)
+        movie.delete()
         return '', 200
 
     def get(self, id):
